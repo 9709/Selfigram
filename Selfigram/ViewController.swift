ffd8741865a49058e25bc0b499f0c74fbebe8f8f
Matthew Chan <mat_t4@hotmail.com>
2018-09-26 19:07:59 -0700

    Initial Commit

---
+++ Selfigram/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Selfigram
+//
+//  Created by Matthew Chan on 2018-09-26.
+//  Copyright © 2018 Matthew Chan. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
